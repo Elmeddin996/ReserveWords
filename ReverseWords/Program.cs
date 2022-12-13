ca15c53e9ca3ec652abd73bfd54c11167d310875
elmeddin mirzeyev <elmaddinshm@code.edu.az>
2022-12-14 01:38:10 +0400

    reserve

---
+++ ReverseWords/Program.cs
@@ -0,0 +1,14 @@
+﻿string words =Console.ReadLine();
+string total = "";
+
+foreach (var word in words.Split(' '))
+{
+    string temp = "";
+    foreach (var ch in word.ToCharArray())
+    {
+        temp = ch + temp;
+    }
+    total = total + temp + " ";
+}
+Console.WriteLine(total);
+
